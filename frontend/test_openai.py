eb8ae47aa370f25a0c8a098564c8b17b48c058ca
chikhio123 <2907105643@qq.com>
2026-05-07 14:30:06 +0800

    chore: remove leftover temporary fix scripts

--- frontend/test_openai.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, 'D:/Code/Folder Steward/backend')
-from app.services.llm_provider_service import LLMProviderService
-s = LLMProviderService()
-print(s.generate_classification({'filename':'test'}, [], 'D:/Archive'))
